7ae90c0a8df4f7211729139dac5ba51337c6f0f5
JP <jean.p.ebejer@um.edu.mt>
2022-06-24 16:42:56 +0200

    reupload version with Data and Software section

--- prep_manu.sh
+++ prep_manu.sh
@@ -2,10 +2,14 @@
 
 set -e # fail on error
 
+# remove all existing files, to regenerate
+rm -v ./202206_FewShotLearningForLowDataDrugDiscovery_Vella*
+
 cd ./paper
 rubber --clean main supporting_information
-zip -9r ../202206_FewShotLearningForLowDataDrugDiscovery_Vella_LaTeX.zip ./* -x "./corrections/*" 
+rm main.out 
+zip -9r ../202206_FewShotLearningForLowDataDrugDiscovery_Vella_LaTeX.zip ./* -x "./corrections/*" -x "./img/graphical_abstract.tif"
 cp -v ./supporting_information.pdf ../202206_FewShotLearningForLowDataDrugDiscovery_Vella_SupportingInformation.pdf
 cp -v ./main.pdf ../202206_FewShotLearningForLowDataDrugDiscovery_Vella.pdf
 cp -v ./img/cover_art.png ../202206_FewShotLearningForLowDataDrugDiscovery_Vella_CoverArt.png
-cd ..
+cd .. # take us back to the old dir
