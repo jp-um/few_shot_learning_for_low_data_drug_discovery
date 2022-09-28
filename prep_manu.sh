8f8d60f46ac75fec6ed03b90644590d447195c44
JP <jean.p.ebejer@um.edu.mt>
2022-09-28 17:24:06 +0200

    Submission for first review.

--- prep_manu.sh
+++ prep_manu.sh
@@ -3,13 +3,15 @@
 set -e # fail on error
 
 # remove all existing files, to regenerate
-rm -v ./202206_FewShotLearningForLowDataDrugDiscovery_Vella*
+rm -fv ./202209_ci-2022-00779z_FewShotLearningForLowDataDrugDiscovery_Vella*
 
 cd ./paper
 rubber --clean main supporting_information
-rm main.out 
-zip -9r ../202206_FewShotLearningForLowDataDrugDiscovery_Vella_LaTeX.zip ./* -x "./corrections/*" -x "./img/graphical_abstract.tif"
-cp -v ./supporting_information.pdf ../202206_FewShotLearningForLowDataDrugDiscovery_Vella_SupportingInformation.pdf
-cp -v ./main.pdf ../202206_FewShotLearningForLowDataDrugDiscovery_Vella.pdf
-cp -v ./img/cover_art.png ../202206_FewShotLearningForLowDataDrugDiscovery_Vella_CoverArt.png
+rm -fv main.out 
+zip -9r ../202209_ci-2022-00779z_FewShotLearningForLowDataDrugDiscovery_Vella_LaTeX.zip ./* -x "./review/*" -x "./submissions/*" -x "./corrections/*" -x "./img/graphical_abstract.tif"
+cp -v ./supporting_information.pdf ../202209_ci-2022-00779z_FewShotLearningForLowDataDrugDiscovery_Vella_SupportingInformation.pdf
+cp -v ./main.pdf ../202209_ci-2022-00779z_FewShotLearningForLowDataDrugDiscovery_Vella.pdf
+cp -v ./img/cover_art.png ../202209_ci-2022-00779z_FewShotLearningForLowDataDrugDiscovery_Vella_TableOfContentsGraphic.png
+cp -v ./coverpage_art/ci-2022-00779z-coverpage-art.png ../202209_ci-2022-00779z_FewShotLearningForLowDataDrugDiscovery_Vella_FrontCover.png
+cp -v ./main-diff-rev1.pdf ../202209_ci-2022-00779z_FewShotLearningForLowDataDrugDiscovery_Vella_AnnotatedChanges.pdf
 cd .. # take us back to the old dir
